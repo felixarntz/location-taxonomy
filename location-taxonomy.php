07b9d588daad9595e6ff03c177d9d97862977b44
Felix Arntz <flixos90@gmail.com>
2026-08-03 22:42:18 -0500

    raise minimum required PHP version to 7.4

--- location-taxonomy.php
+++ location-taxonomy.php
@@ -11,7 +11,7 @@
  * Description: Registers a hierarchical taxonomy to associate your posts with locations.
  * Version: 1.0.0
  * Requires at least: 6.0
- * Requires PHP: 7.2
+ * Requires PHP: 7.4
  * Author: Felix Arntz
  * Author URI: https://felix-arntz.me
  * License: GPLv2 or later
